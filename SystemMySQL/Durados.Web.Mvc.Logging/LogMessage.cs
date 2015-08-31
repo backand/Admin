52640bd64a6d8fcad245ac6f3778d091487c5bd9
yariv <yariv@e70d89a3-24dd-45e3-ab19-0cf0436ba74b>
2015-08-31 13:36:03 +0000

    integrate with elastic search

---
+++ SystemMySQL/Durados.Web.Mvc.Logging/LogMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Durados.Web.Mvc.Logging
+{
+   
+    public class LogMessage
+    {
+        public string ID { get; set; }
+        public string ApplicationName { get; set; }
+        public string Username { get; set; }
+        public string MachineName { get; set; }
+        public string Time { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public string MethodName { get; set; }
+        public string LogType { get; set; }
+        public string ExceptionMessage { get; set; }
+        public string Trace { get; set; }
+        public string FreeText { get; set; }
+        public string Guid { get; set; }
+    }
+}
