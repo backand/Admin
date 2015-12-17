bb067685360b729dd44e35f46f2a5c902dee8aa9
relly <relly@backand.com>
2015-12-17 17:55:36 +0200

    add missing file

---
+++ Durados.Web.Mvc/UI/Helpers/IFarmCaching.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Durados.Web.Mvc.UI.Helpers
+{
+    public interface IFarmCaching
+    {
+        void ClearInternalAddresses();
+
+        void ClearInternalCache(string appName);
+
+        void ClearMachinesCache(string appName, bool async = false);
+
+        void AppStarted();
+
+        void ClearMachinesAddresses();
+
+        void AppEnded();
+    }
+
+    public class DummyFarmCaching : IFarmCaching
+    {
+        public void ClearInternalAddresses()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void ClearInternalCache(string appName)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void ClearMachinesCache(string appName, bool async = false)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void AppStarted()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void ClearMachinesAddresses()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void AppEnded()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+
+}
