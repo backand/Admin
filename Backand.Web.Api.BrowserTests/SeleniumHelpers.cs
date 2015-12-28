798a6ec69cd6c2cd043479fa0f68bc9f449c98d1
relly <relly@backand.com>
2015-12-28 17:02:37 +0200

    selenium test and app not ready yet fix

---
+++ Backand.Web.Api.BrowserTests/SeleniumHelpers.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+
+namespace Backand.Web.Api.BrowserTests
+{
+    public static class SeleniumHelpers
+    {
+        public static void MoveTo(this IWebDriver driver,By locator)
+        {
+            var element = driver.FindElement(locator);
+            Actions actions = new Actions(driver);
+            actions.MoveToElement(element);
+            actions.Perform();
+        }
+
+        
+    }
+}
