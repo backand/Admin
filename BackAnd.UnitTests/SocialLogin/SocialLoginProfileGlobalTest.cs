e4d33ddab1ff972ba04df3ff75ee455d12ab23d2
Ygal Bellaiche <ygalbel@gmail.com>
2016-03-17 17:59:26 +0200

    Add test  to social login

---
+++ BackAnd.UnitTests/SocialLogin/SocialLoginProfileGlobalTest.cs
@@ -0,0 +1,27 @@
+﻿using Durados.Web.Mvc.SocialLogin;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace BackAnd.UnitTests
+{
+    [TestClass]
+    public class SocialLoginProfileGlobalTest
+    {
+        [TestMethod]
+        public void TestGetProfileReturnProfile()
+        {
+            var provider = SocialProviderFactory.GetSocialProvider("google");
+            provider.MockReturnFromService = new Dictionary<string, object>
+            { { "testKey" , "testValue" },
+                {"id" , "idValue" } };
+
+            var result = provider.GetProfile("appName", "accessToken", "email@gmail.com");
+
+            Assert.AreEqual(result.appName, "appName");
+            Assert.AreEqual(result.email, "email@gmail.com");
+            Assert.AreEqual(result.id, "idValue");
+            //Assert.AreEqual(result.returnAddress, "dummy");
+        }
+
+    }
+}
