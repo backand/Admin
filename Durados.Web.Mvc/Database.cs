fdc8e45338eae95ee219265bf140f909dd1d652e
relly <relly@backand.com>
2016-04-05 16:05:36 +0300

    fix cast view error

--- Durados.Web.Mvc/Database.cs
+++ Durados.Web.Mvc/Database.cs
@@ -1367,9 +1367,13 @@ namespace Durados.Web.Mvc
         {
             ArrayList list = new ArrayList();
 
-            foreach (View view in Views.Values.Where(v => !v.SystemView).OrderBy(v => v.Order))
+            foreach (var view1 in Views.Values.Where(v => !v.SystemView).OrderBy(v => v.Order))
             {
-                list.Add(new Dictionary<string, object>() { { "name", view.JsonName }, { "fields", view.GetModelFieldsTypes() } });
+                if (view1 is View)
+                {
+                    View view = (View)view1;
+                    list.Add(new Dictionary<string, object>() { { "name", view.JsonName }, { "fields", view.GetModelFieldsTypes() } });
+                }
             }
 
             return list;
