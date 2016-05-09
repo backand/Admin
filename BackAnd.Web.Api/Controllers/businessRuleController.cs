69e7a584caf79f3bc83e75d36c63e25a98187709
relly <relly@backand.com>
2016-05-09 18:35:08 +0300

    handle fileName not exists

--- BackAnd.Web.Api/Controllers/businessRuleController.cs
+++ BackAnd.Web.Api/Controllers/businessRuleController.cs
@@ -501,6 +501,10 @@ namespace BackAnd.Web.Api.Controllers
             }
 
             string actionName = e.Values[Name].ToString();
+            if (fileName == null)
+            {
+                fileName = actionName + ".zip";
+            }
             string functionName = Map.AppName + "_" + viewName + "_" + actionName;
             string folder = Map.AppName + "/" + viewName + "/" + actionName;
             nodeJS.Create(Maps.NodeJSBucket, folder, fileName, functionName, "handler", "handler");
