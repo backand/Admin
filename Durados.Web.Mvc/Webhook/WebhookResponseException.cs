8cc0915dee516cbbcf7652346eba48335ca52b44
relly <relly@backand.com>
2016-06-07 11:32:53 +0300

    WebhookResponseException

---
+++ Durados.Web.Mvc/Webhook/WebhookResponseException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Durados.Web.Mvc.Webhook
+{
+    public class WebhookResponseException : DuradosException
+    {
+        public WebhookResponseException(string message)
+            : base(message)
+        {
+
+        }
+    }
+}
