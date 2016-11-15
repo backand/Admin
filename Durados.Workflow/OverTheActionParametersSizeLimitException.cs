7dc4b3c49f87f73f026d39878760b6f7f0303783
relly <relly@backand.com>
2016-11-15 10:08:32 +0200

    parameters size limition message typo

--- Durados.Workflow/OverTheActionParametersSizeLimitException.cs
+++ Durados.Workflow/OverTheActionParametersSizeLimitException.cs
@@ -8,7 +8,7 @@ namespace Durados.Workflow
     public class OverTheActionParametersSizeLimitException : Durados.DuradosException
     {
         public OverTheActionParametersSizeLimitException(int sizeLimit, int parametersSize)
-            : base("The action parameters size are limited to " + sizeLimit + " KB. The current parameters size is " + parametersSize + " KB. Either reduce the parameters size or contact the administrator.")
+            : base("The action parameters size is limited to " + sizeLimit + " KB. The current parameters size is " + parametersSize + " KB. Either reduce the parameters size or contact the administrator.")
         {
 
         }
