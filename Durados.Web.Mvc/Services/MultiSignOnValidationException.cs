2c6a3fc03a19b4cffffb321a307d1341719b7afb
relly <relly@backand.com>
2017-01-24 15:06:43 +0200

    missing file

---
+++ Durados.Web.Mvc/Services/MultiSignOnValidationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Durados.Web.Mvc.Services
+{
+    public class MultiSignOnValidationException : Exception
+    {
+        public MultiSignOnValidationException(Exception innerException) : base("Failed to compare password", innerException)
+        {
+
+        }
+    }
+}
