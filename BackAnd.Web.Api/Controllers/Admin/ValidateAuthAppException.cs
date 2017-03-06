cb6cfa8ef44aa94ead2c3167c03b3e4a942fcd05
relly <relly@backand.com>
2017-03-06 15:00:37 +0200

    missing file

---
+++ BackAnd.Web.Api/Controllers/Admin/ValidateAuthAppException.cs
@@ -0,0 +1,17 @@
+﻿using Durados;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BackAnd.Web.Api.Controllers.Admin
+{
+    public class ValidateAuthAppException : DuradosException
+    {
+        public ValidateAuthAppException(string message)
+            : base(message)
+        {
+
+        }
+    }
+}
