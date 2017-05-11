57ce330d028b076be24c12e9d6e0906f5d17c4ba
relly <relly@backand.com>
2017-05-11 10:54:20 +0300

    missing files

---
+++ Durados.Web.Mvc/UI/Helpers/LambdaFunctionSelectionAlreadyExists.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Durados.Web.Mvc.UI.Helpers
+{
+    public class LambdaFunctionSelectionAlreadyExists : DuradosException
+    {
+        public LambdaFunctionSelectionAlreadyExists(string name)
+            : base("The function " + name + " already exists.")
+        {
+
+        }
+    }
+}
---
+++ Durados.Web.Mvc/UI/Helpers/LambdaFunctionSelectionNotFound.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Durados.Web.Mvc.UI.Helpers
+{
+    public class LambdaFunctionSelectionNotFound : DuradosException
+    {
+        public LambdaFunctionSelectionNotFound(string name)
+            : base("The function " + name + " was not found.")
+        {
+
+        }
+    }
+}
