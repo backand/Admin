6c1ec63e7cbe74ac0a632fe7ec840443457e7ab6
relly <relly@backand.com>
2017-05-24 17:20:39 +0300

    handle function response

--- Durados.Workflow/NodeJsException.cs
+++ Durados.Workflow/NodeJsException.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 
 namespace Durados.Workflow
 {
     public class NodeJsException : Durados.DuradosException
     {
-        public NodeJsException(string message)
+        public bool JsonFormat { get; private set; }
+        public HttpStatusCode HttpStatusCode { get; private set; }
+        public NodeJsException(string message, bool jsonFormat = false, HttpStatusCode httpStatusCode = HttpStatusCode.InternalServerError)
             : base(message)
         {
-
+            JsonFormat = jsonFormat;
+            HttpStatusCode = httpStatusCode;
         }
     }
 }
