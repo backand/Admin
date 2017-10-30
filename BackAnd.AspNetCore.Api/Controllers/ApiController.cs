8e8c3ea2e30104d19d33bf332908890791e224ce
relly <relly@backand.com>
2017-10-30 16:41:08 +0200

    core startup

---
+++ BackAnd.AspNetCore.Api/Controllers/ApiController.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using Durados.Web.Mvc;
+using Durados.Web.Mvc.UI.Helpers;
+using System.Collections;
+using Newtonsoft.Json;
+
+namespace BackAnd.AspNetCore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ApiController : Controller
+    {
+        protected Map map = null;
+        public Map Map
+        {
+            get
+            {
+                if (map == null)
+                    map = Maps.Instance.GetMap();
+                map.OpenSshSession();
+                return map;
+            }
+        }
+
+        protected internal virtual Durados.Web.Mvc.View GetView(string viewName)
+        {
+            Durados.Web.Mvc.View view = (Durados.Web.Mvc.View)Map.Database.GetViewByJsonName(viewName);
+            if (view != null)
+                return view;
+
+            return ViewHelper.GetViewForRest(viewName);
+        }
+
+        protected virtual bool IsAllow(View view)
+        {
+            if (view.SystemView && view.Database.Map.IsMainMap)
+            {
+                return false;
+            }
+            if (Map.Database.GetCurrentUsername() == Durados.Database.GuestUsername && (view.Database.IsConfig || view.SystemView))
+                return false;
+            return view.IsAllow();
+        }
+
+        protected void ReplaceVariables(Dictionary<string, object> result)
+        {
+            if (result.ContainsKey("values") && result.ContainsKey("variables") && result.ContainsKey("str") && result.ContainsKey("where"))
+            {
+                foreach (string variable in (ArrayList)result["variables"])
+                {
+                    string value = ((Dictionary<string, object>)result["values"])[variable].ToString();
+                    if (!(value.StartsWith("'") && value.EndsWith("'")))
+                    {
+                        decimal d = 0;
+                        if (!decimal.TryParse(value, out d))
+                        {
+                            value = value.Pad("'");
+                        }
+                    }
+                    result["str"] = result["str"].ToString().Replace(variable, value);
+                    result["where"] = result["where"].ToString().Replace(variable, value);
+                }
+            }
+        }
+
+        protected virtual Dictionary<string, object> transformJson(string json, bool shouldGeneralize = false)
+        {
+            string getNodeUrl = GetNodeUrl() + "/transformJson";
+
+            bulk bulk = new Durados.Web.Mvc.UI.Helpers.bulk();
+
+            var data = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+
+            if (!data.ContainsKey("appName"))
+            {
+                string appName = Map.AppName;
+                if (string.IsNullOrEmpty(appName))
+                {
+                    appName = (System.Web.HttpContext.Current.Items[Durados.Web.Mvc.Database.AppName] ?? string.Empty).ToString();
+                }
+                data.Add("appName", appName);
+            }
+            if (shouldGeneralize)
+            {
+                data.Add("shouldGeneralize", shouldGeneralize);
+            }
+
+            json = jss.Serialize(data);
+
+            var tasks = new List<Task<string>>();
+            object responses = null;
+            tasks.Add(Task.Factory.StartNew(() =>
+            {
+                Dictionary<string, object> headers = new Dictionary<string, object>() { { "Content-Type", "application/json" } };
+
+                // in transformJson take app creator credential to allow get BakandToObject
+                headers.Add("Authorization", Map.GetAuthorization());
+                var responseStatusAndData = bulk.GetWebResponse("POST", getNodeUrl, json, null, headers, 0);
+                responses = responseStatusAndData.data;
+                if (string.IsNullOrEmpty(responseStatusAndData.data))
+                {
+                    if (responseStatusAndData.GetHeaders()["error"] != null && !string.IsNullOrEmpty(responseStatusAndData.GetHeaders()["error"].ToString()))
+                    {
+                        throw new DuradosException(responseStatusAndData.GetHeaders()["error"].ToString());
+                    }
+                }
+                return responseStatusAndData.data;
+            }));
+
+            Task.WaitAll(tasks.ToArray());
+
+            Dictionary<string, object> result = null;
+            try
+            {
+                result = jss.Deserialize<Dictionary<string, object>>(responses.ToString());
+            }
+            catch
+            {
+                throw new DuradosException(responses.ToString());
+            }
+
+            if (result == null)
+            {
+                LogModel(json, string.Empty, string.Empty, "nosql");
+            }
+            else
+            {
+                LogModel(json, new JavaScriptSerializer().Serialize(result), result.ContainsKey("valid") ? result["valid"].ToString() : string.Empty, "nosql");
+            }
+
+            return result;
+
+
+
+        }
+
+    }
+
+    public class Messages
+    {
+        public static readonly string FieldShouldBeParent = "The field \"{0}\" nust be a single-select or a multi-select type.";
+        public static readonly string ForeignKeyDeleteViolation = "The row you are trying to delete is referenced in another table ,please check your table constraints";
+        public static readonly string FieldShouldBeAutoComplete = "The field \"{0}\" nust be an autocomplete display format.";
+        public static readonly string ViewNameIsMissing = "The object name is missing.";
+        public static readonly string FieldNameIsMissing = "The field name is missing.";
+        public static readonly string IdIsMissing = "The id is missing.";
+        public static readonly string CollectionIsMissing = "The collection is missing.";
+        public static readonly string CollectionNotFound = "The collection was not found.";
+        public static readonly string DuplicateCollectionName = "The collection name exists more than once.";
+        public static readonly string ViewIsUnauthorized = "The object is unauthorized for this current user role.";
+        public static readonly string ViewNameNotFound = "The object \"{0}\" was not found.";
+        public static readonly string PostContradictsPredefinedFilter = "Post failed because it contradicts the predefined filter.";
+
+        public static readonly string MissingObjectToUpdate = "The object to update is missing.";
+        public static readonly string FieldNameNotFound = "The field \"{0}\" was not found.";
+        public static readonly string TheFieldMustBeTextual = "The field must be textual.";
+        public static readonly string ItemWithIdNotFound = "An item with id \"{0}\" was either not found, not allowed to the current role or filtered out by a predefined filter in the object \"{1}\".";
+        public static readonly string ItemWithNoFieldsToUpdate = "An item with id \"{0}\" has no fields to update in the object \"{1}\".";
+        public static readonly string AppNotFound = "The app \"{0}\" was not found.";
+        public static readonly string ChartWithIdNotFound = "An chart with id \"{0}\" was not found.";
+        public static readonly string Unexpected = "An error occurred, please try again or contact the administrator. Error details: {0}";
+        public static readonly string Critical = "Critical Exception";
+        public static readonly string ActionIsUnauthorized = "The action is unauthorized for this current user role.";
+        public static readonly string WorkspaceNotFound = "The workspace was not found";
+        public static readonly string WorkspaceNameMissing = "The workspace name is missing";
+        public static readonly string WorkspaceWithNameAlreadyExists = "A Workspace with the name {0} already exists.";
+        public static readonly string QueryWithNameAlreadyExists = "A Query with the name {0} already exists.";
+        public static readonly string CronWithNameAlreadyExists = "A Cron with the name {0} already exists.";
+        public static readonly string UploadWithNameAlreadyExists = "An Upload with the name {0} already exists.";
+        public static readonly string ChangeAdminWorkspaceNameNotAllowed = "Changing Admin worksapces name is not allowed.";
+        public static readonly string WorkspaceLimit = "You have reached workspaces limit";
+        public static readonly string UploadNotFound = "The column has no upload configuration";
+        public static readonly string InvalidFileType = "Invalid file type";
+        public static readonly string InvalidFileType2 = "Invalid file type in field [{0}].<br><br>Allowed formats: {1}";
+        public static readonly string AppNameAlreadyExists = "An application by the name {0} already exists.";
+        public static readonly string AppNameCannotBeNull = "App name cannot be empty.";
+        public static readonly string AppNameInvalid = "App name must be alphanumeric.";
+        public static readonly string ActionWithNameAlreadyExists = "An action with the name {0} already exists for table {1}.";
+        public static readonly string FunctionWithNameAlreadyExists = "A function with the name {0} already exists.";
+        public static readonly string RuleNotFound = "The action does not exist.";
+        public static readonly string NotImplemented = "The action is not implemented yet.";
+        public static readonly string FailedToGetJsonFromParameters = "Failed to get json from parameters.";
+        public static readonly string StringifyFilter = "Please JSON.stringify the filter parameter";
+        public static readonly string StringifyBulk = "Failed to parse the Bulk JSON";
+        public static readonly string GetFilterError = "Failed to translate filter";
+        public static readonly string StringifySort = "Please JSON.stringify the sort parameter";
+        public static readonly string StringifyFields = "Please provide the fields parameters as ['field1','field2']";
+        public static readonly string InvalidSchema = "Invalid schema";
+
+        public static readonly string MoreThanOneParseConversions = "{0} has more than one parse conversions.";
+        public static readonly string MigrationAlreadyStartedWithoutGettingItsStatus = "{0} has already created a migration request.";
+        public static readonly string MigrationAlreadyStartedWithStatusIdle = "{0} has already created a migration request and it is now waiting to start.";
+        public static readonly string MigrationAlreadyStartedWithStatusStarted = "{0} has already started its migration.";
+        public static readonly string MigrationAlreadyStartedWithStatusFinished = "{0} has already finished its migration. If you want to migrate again please create a new app.";
+        public static readonly string NotSignInToApp = "Please sign in to an app";
+        public static readonly string CloudVendorNotFound = "This cloud vendor is not currently supported";
+        public static readonly string CloudTypeNotFound = "This cloud service type is not currently supported";
+        public static readonly string DuplicateCloudName = "This account name is already in use";
+        public static readonly string MissingCloudName = "Please provide the account name";
+
+    }
+}
---
+++ BackAnd.AspNetCore.Api/Controllers/ViewDataController.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Durados.Web.Mvc;
+using Durados.DataAccess;
+using Durados.Web.Mvc.UI.Helpers;
+using Newtonsoft.Json;
+
+namespace BackAnd.AspNetCore.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("1/objects")]
+    public class ViewDataController : ApiController
+    {
+        string where = null;
+        bool isNosqlFilter = false;
+
+        private bool IsNosqlFilter(string filter)
+        {
+            return !string.IsNullOrEmpty(filter) && filter.Contains("\"q\":");
+        }
+
+        private Dictionary<string, object> GetWhereFromCache(View view, string json)
+        {
+            return NoSqlFilterCache.Get(Map.AppName, view, json);
+        }
+
+        private string GetWhere(View view, string json, bool useCache = false)
+        {
+            if (useCache)
+            {
+                try
+                {
+                    var cacheResponse = GetWhereFromCache(view, json);
+
+                    if (cacheResponse != null)
+                    {
+                        ReplaceVariables(cacheResponse);
+                        return cacheResponse["where"].ToString();
+                    }
+                }
+                catch { }
+            }
+
+            var data = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+
+            if (!(data.Count == 1 && data.ContainsKey("q")))
+            {
+                throw new Durados.DuradosException("filter must start with \"q\"");
+            }
+
+            if (!data.ContainsKey("object"))
+            {
+                var data2 = new Dictionary<string, object>();
+                data2.Add("object", view.JsonName);
+                data2.Add("q", data["q"]);
+                data = data2;
+            }
+
+
+            if (!data.ContainsKey("json"))
+            {
+                var data2 = new Dictionary<string, object>();
+                data2.Add("json", data);
+                data = data2;
+            }
+
+            data.Add("isFilter", true);
+            
+            json = JsonConvert.SerializeObject(data);
+
+            var response = transformJson(json, useCache);
+
+            if (!response.ContainsKey("where"))
+            {
+                throw new Durados.DuradosException("Failed to transform nosql filter");
+            }
+
+            if (useCache)
+            {
+                SetWhereToCache(view, json, response);
+            }
+
+            if (useCache)
+            {
+                ReplaceVariables(response);
+            }
+
+
+            return response["where"].ToString();
+
+        }
+
+
+        // GET: api/viewData
+        [HttpGet("{name}")]
+        public ObjectResult Get(string name, bool? withSelectOptions = null, bool? withFilterOptions = null, int? pageNumber = null, int? pageSize = null, string filter = null, string sort = null, string search = null, bool? deep = null, bool descriptive = true, bool? relatedObjects = false, string exclude = null, string fields = null)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(name))
+                {
+                    return NotFound(Messages.ViewNameIsMissing);
+                }
+                View view = GetView(name);
+                if (view == null)
+                {
+                    return NotFound(string.Format(Messages.ViewNameNotFound, name));
+                }
+                if (!IsAllow(view))
+                {
+                    return StatusCode(StatusCodes.Status403Forbidden, Messages.ViewIsUnauthorized);
+                }
+
+                int rowCount = 0;
+
+                Dictionary<string, object>[] filterArray = null;
+                isNosqlFilter = IsNosqlFilter(filter);
+                if (isNosqlFilter)
+                {
+                    where = GetWhere(view, filter);
+                }
+
+                else if (!string.IsNullOrEmpty(filter) && filter != "filter" && filter != "false" && filter != "null" && filter != "undefined" && filter != "[{}]")
+                {
+                    if (filter == "(Collection)")
+                    {
+                        filter = "[" + System.Web.HttpContext.Current.Request.Params["filter"] + "]";
+                    }
+                    if (filter.StartsWith("{"))
+                    {
+                        filter = "[" + filter + "]";
+                    }
+                    try
+                    {
+                        filterArray = JsonConverter.DeserializeArray(filter);
+                    }
+                    catch (Exception exception)
+                    {
+                        Map.Logger.Log(GetControllerNameForLog(this.ControllerContext), "get", exception.Source, exception, 1, "Deserialize filter " + filter + ", original: " + System.Web.HttpContext.Current.Request.Params["filter"]);
+                        return ResponseMessage(Request.CreateResponse(HttpStatusCode.NotAcceptable, Messages.StringifyFilter));
+                    }
+                }
+
+                Dictionary<string, object>[] sortArray = null;
+
+                if (!string.IsNullOrEmpty(sort) && sort != "sort" && sort != "false" && sort != "null" && sort != "undefined" && sort != "[{}]")
+                {
+                    if (sort == "(Collection)")
+                    {
+                        sort = "[" + System.Web.HttpContext.Current.Request.Params["sort"] + "]";
+                    }
+                    if (sort.StartsWith("{"))
+                    {
+                        sort = "[" + sort + "]";
+                    }
+                    try
+                    {
+                        sortArray = JsonConverter.DeserializeArray(sort);
+                    }
+                    catch (Exception exception)
+                    {
+                        Map.Logger.Log(GetControllerNameForLog(this.ControllerContext), "get", exception.Source, exception, 1, "Deserialize sort " + sort + ", original: " + System.Web.HttpContext.Current.Request.Params["sort"]);
+                        return ResponseMessage(Request.CreateResponse(HttpStatusCode.NotAcceptable, Messages.StringifySort));
+                    }
+                }
+
+                if (search == "null" || search == "undefined")
+                    search = null;
+
+                string[] fieldsArray = null;
+                if (!string.IsNullOrEmpty(fields) && fields != "fields" && fields != "all" && fields != "false" && fields != "null" && fields != "undefined" && fields != "[]")
+                {
+                    if (!(fields.Contains('\'') || fields.Contains('\"')))
+                    {
+                        return ResponseMessage(Request.CreateResponse(HttpStatusCode.NotAcceptable, Messages.StringifyFields));
+                    }
+                    try
+                    {
+                        fieldsArray = JsonConverter.DeserializeStringArray(fields);
+                    }
+                    catch (Exception exception)
+                    {
+                        Map.Logger.Log(GetControllerNameForLog(this.ControllerContext), "get", exception.Source, exception, 1, "Deserialize fields " + fields + ", original: " + System.Web.HttpContext.Current.Request.Params["fields"]);
+                        return ResponseMessage(Request.CreateResponse(HttpStatusCode.NotAcceptable, Messages.StringifyFields));
+                    }
+                }
+
+
+                bool hideMetadata = exclude != null && exclude.ToLower().Contains("metadata");
+                bool hideTotalRows = exclude != null && exclude.ToLower().Contains("totalrows");
+                var items = RestHelper.Get(view, withSelectOptions ?? false, withFilterOptions ?? false, pageNumber ?? 1, pageSize ?? view.Database.DefaultPageSize, filterArray, search, sortArray, out rowCount, deep ?? false, view_BeforeSelect, view_AfterSelect, false, descriptive, false, relatedObjects ?? false, where, hideMetadata, hideTotalRows, fieldsArray);
+
+                return Ok(items);
+
+            }
+            catch (FilterException exception)
+            {
+                return ResponseMessage(Request.CreateResponse(HttpStatusCode.NotAcceptable, exception.Message));
+            }
+            catch (SortException exception)
+            {
+                return ResponseMessage(Request.CreateResponse(HttpStatusCode.NotAcceptable, exception.Message));
+            }
+            catch (System.Data.Common.DbException exception)
+            {
+                return ResponseMessage(Request.CreateResponse(HttpStatusCode.NotAcceptable, "Check the security Pre-defined Filter for the following sql errors: " + exception.Message));
+
+            }
+            catch (AggregateException exception)
+            {
+                if (exception.InnerException != null)
+                    throw new BackAndApiUnexpectedResponseException(exception.InnerException, this);
+                else
+                    throw new BackAndApiUnexpectedResponseException(exception, this);
+            }
+            catch (Exception exception)
+            {
+                throw new BackAndApiUnexpectedResponseException(exception, this);
+
+            }
+        }
+
+        // GET: api/viewData/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/viewData
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/viewData/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
